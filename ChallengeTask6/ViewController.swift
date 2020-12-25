533bc9a8d5b91f842bc89fe3df58b2a89e844e4c
ksy781 <k.s.k.s.hanahana@gmail.com>
2020-12-25 15:12:32 +0900

    Initial Commit

---
+++ ChallengeTask6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChallengeTask6
+//
+//  Created by 酒井健太郎 on 2020/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
