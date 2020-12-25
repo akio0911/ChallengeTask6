83383b45c8f249c6ebf04f57411ba1fc02c4918f
ksy781 <k.s.k.s.hanahana@gmail.com>
2020-12-25 17:14:49 +0900

    Change slider maxvalue and Add localizedStringWithFormat

--- ChallengeTask6/ViewController.swift
+++ ChallengeTask6/ViewController.swift
@@ -15,7 +15,7 @@ final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         slider.minimumValue = 1
-        slider.maximumValue = 100
+        slider.maximumValue = 100000
     }
     
     @IBAction func randomButton(_ sender: Any) {
@@ -29,8 +29,8 @@ final class ViewController: UIViewController {
     private func presentAlert(message: String) {
         let alert = UIAlertController(title: "結果", message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "再挑戦", style: .default, handler: {(UIAlertAction) in
-                                        let num = Int.random(in: 1...100)
-                                        self.label.text = String(num)}))
+                                        let num = Int.random(in: 1...100000)
+                                        self.label.text = String.localizedStringWithFormat("%d", num)}))
         present(alert, animated: true, completion: nil)
     }
     
