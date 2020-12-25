c34e8c782a7670c44ab9fd5da33946da79bf42a6
ksy781 <k.s.k.s.hanahana@gmail.com>
2020-12-25 16:29:14 +0900

    Add slider

--- ChallengeTask6/ViewController.swift
+++ ChallengeTask6/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet private weak var slider: UISlider!
+    @IBOutlet private weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        slider.minimumValue = 1
+        slider.maximumValue = 100
     }
-
-
+    
+    @IBAction func randomButton(_ sender: Any) {
+        if label.text == String(Int(slider.value)) {
+            presentAlert(message: "あたり\nあなたの値は\(Int(slider.value))です")
+        } else {
+            presentAlert(message: "ハズレ\nあなたの値は\(Int(slider.value))です")
+        }
+    }
+    
+    private func presentAlert(message: String) {
+        let alert = UIAlertController(title: "結果", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "再挑戦", style: .default, handler: {(UIAlertAction) in
+                                        let num = Int.random(in: 0...100)
+                                        self.label.text = String(num)}))
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
-
