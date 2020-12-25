4a9c271d4a68422dcdc59859bf4458ba98c6a650
akio0911 <akio0911@gmail.com>
2020-12-25 21:46:49 +0900

    フィードバック

--- ChallengeTask6/ViewController.swift
+++ ChallengeTask6/ViewController.swift
@@ -12,27 +12,57 @@ final class ViewController: UIViewController {
     @IBOutlet private weak var slider: UISlider!
     @IBOutlet private weak var label: UILabel!
     
+    private let answerRange = (1...100_000)
+    
+    private var answer: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        slider.minimumValue = 1
-        slider.maximumValue = 100000
+        
+        slider.minimumValue = Float(answerRange.lowerBound)
+        slider.maximumValue = Float(answerRange.upperBound)
+        
+        makeAnswer()
+        displayAnswer()
     }
     
     @IBAction func randomButton(_ sender: Any) {
-        let sliderValue = String.localizedStringWithFormat("%d", Int(slider.value))
-        if label.text == sliderValue {
-            presentAlert(message: "あたり\nあなたの値は\(sliderValue)です")
+        let sliderValue = Int(slider.value)
+        
+        let firstLine: String
+        
+        if answer == sliderValue {
+            firstLine = "あたり"
         } else {
-            presentAlert(message: "ハズレ\nあなたの値は\(sliderValue)です")
+            firstLine = "ハズレ"
         }
+        
+        presentAlert(message: "\(firstLine)\nあなたの値は\(sliderValue)です")
     }
     
     private func presentAlert(message: String) {
         let alert = UIAlertController(title: "結果", message: message, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "再挑戦", style: .default, handler: {(UIAlertAction) in
-                                        let num = Int.random(in: 1...100000)
-                                        self.label.text = String.localizedStringWithFormat("%d", num)}))
+        
+        alert.addAction(
+            UIAlertAction(
+                title: "再挑戦",
+                style: .default,
+                handler: { [weak self] _ in
+                    guard let strongSelf = self else { return }
+                    strongSelf.makeAnswer()
+                    strongSelf.displayAnswer()
+                }
+            )
+        )
+        
         present(alert, animated: true, completion: nil)
     }
+
+    private func makeAnswer() {
+        answer = Int.random(in: answerRange)
+    }
     
+    private func displayAnswer() {
+        label.text = String.localizedStringWithFormat("%d", answer)
+    }
 }
